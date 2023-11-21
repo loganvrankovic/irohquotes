92662f6474b944ea47aaa078dae5c1105b676ad1
Logan Vrankovic <loganvrz@gmail.com>
2023-11-20 21:40:11 -0500

    improved (added) info window (it wasn't even a window)

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Uncle Iroh Quotes  
 
-Random quotes from Uncle Iroh! You can:
-
-- Learn about different Asian tea leaves  
-
-- Tweet your favorite quotes
-
-- Change your theme, and share the URL  
-
-At the top right of the quote box, listen to Iroh share his words of wisdom, or refresh the quote to see a new piece of advice with a different mood.  
+Random quotes from Uncle Iroh. At the top right of the quote box, listen to Iroh share his words of wisdom, or refresh the quote to see a new piece of advice with a different mood.  
 This website exists as one of my favorite projects I started while exploring different coding challenge websites, this one being from the freeCodeCamp final project challenges.  
+
+- Learn about different Asian tea leaves.
+
+- Tweet your favorite quotes.
+
+- Change your theme, and share the URL.
 
 ![demo-iroh](https://github.com/loganvrankovic/irohquotes/assets/145373422/ba5d8856-623a-4ec4-9366-6010cc6e83b8)
--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -2,16 +2,20 @@
 import * as React from "react"
 import { useState, useRef } from "react"
 import Link from "next/link"
-import Help from "../components/instructions"
+// import Help from "../components/instructions"
 import teaRand from "../components/tea"
 import quoteRand from "../components/quotes"
 import ModeToggle  from "@/components/modetoggle"
 import { Coffee, 
     Twitter, 
+    Github,
     Quote, 
     Link2, 
     RotateCw, 
-    Headphones } from "lucide-react"
+    Headphones,
+    XCircle,
+    Sun,
+    Info } from "lucide-react"
 import { Button } from "./ui/button"
 import { Card, 
     CardContent,
@@ -63,62 +67,147 @@ function CopyURLButton() {
     )
 }
 
-// random quote div -------------------------------------------------
-export default function RQuote() {
-const [quoteState, setQuoteState] = useState(quoteRand());
-const [bgColor, setBgColor] = useState<string>("#7E9181")
-const [isFading, setIsFading] = useState(false);
-
-const refreshQuote = () => {
-    setIsFading(true)
-
-    setTimeout(() => {
-        const newQuote = quoteRand();
-        const newBgColor = getColor();
-        setBgColor(newBgColor);
-        setQuoteState(newQuote);
-        setIsFading(false);
-    }, 500)
+interface HelpProps {
+    onClick: () => void;
 }
 
-const getColor = () => {
-    return quoteState[1] === "public/irohmp3/iroh0.mp3" ? "#2E3532" :
-       quoteState[1] === "irohmp3/iroh1.mp3" ? "#C7CEDB" :
-       quoteState[1] === "irohmp3/iroh2.mp3" ? "#94849B" :
-       quoteState[1] === "irohmp3/iroh3.mp3" ? "#A0AAB2" :
-       quoteState[1] === "irohmp3/iroh4.mp3" ? "#d5ff80" :
-       quoteState[1] === "irohmp3/iroh5.mp3" ? "#242F40" :
-       quoteState[1] === "irohmp3/iroh6.mp3" ? "#F9E784" :
-       quoteState[1] === "irohmp3/iroh7.mp3" ? "#685044" :
-       quoteState[1] === "irohmp3/iroh8.mp3" ? "#8075FF" :
-       quoteState[1] === "irohmp3/iroh9.mp3" ? "#E5D1D0" :
-       quoteState[1] === "irohmp3/iroh10.mp3" ? "#ccccff" :
-       "#2A2C24";
-}
-
-interface AudioPlayerProps {
-    audioFile: string;
-}
-
-const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioFile }) => {
-    const audioRef = useRef<HTMLAudioElement>(null);
-
-    const playAudio = () => {
-        console.log(audioRef.current)
-        if (audioRef.current) {
-            audioRef.current.play();
-        }
+function Help({onClick}: HelpProps) {
+    const handleClick = () => {
+        onClick();
     }
-
+    
     return (
         <div>
-            <audio ref={audioRef} src={audioFile} />
-            <Button onClick={playAudio} className="share head absolute top-5" variant="outline">
-                <Headphones className="h-5 w-5" />
-            </Button>
+            <p
+            style={{ textDecoration: "underline", cursor: "pointer" }}
+            // onClick={handleHelpClick}
+            className="flex items-center"
+            onClick={handleClick}
+            >
+                <Info size="14px" className="mr-1" />
+                info
+            </p>
         </div>
     )
 }
+
+interface InstructionsCardProps {
+    onClose: () => void;
+}
+
+function InstructionsCard({ onClose }: InstructionsCardProps) {
+    return (
+        <Card className="infocard absolute p-8 shadow-xl mx-3">
+         <CardTitle className=""><p
+            className="flex items-center"
+            >
+                <Info size="16px" className="mr-1" />
+                info
+            </p></CardTitle>
+            <CardDescription className="pt-6">top-right buttons</CardDescription>
+            <CardContent className="pt-2">
+                <p className="flex items-center">
+                    <RotateCw size="14px" className="mr-1"/>
+                    <span className="ml-2">refresh the quote and mood</span>
+                </p>
+                <p className="flex items-center">
+                    <Headphones size="14px" className="mr-1"/>
+                    <span className="ml-2">listen to Iroh&apos;s words of wisdom</span>
+                </p>
+            </CardContent>
+            <CardDescription>bottom-left buttons</CardDescription>
+            <CardContent className="pt-2">
+                <p className="flex items-center">
+                    <Sun size="14px" className="mr-1"/>
+                    <span className="ml-2">change the quote card&apos;s theme</span>
+                </p>
+                <p className="flex items-center">
+                    <Coffee size="14px" className="mr-1"/>
+                    <span className="ml-2">learn about different tea that Iroh enjoys</span>
+                </p>
+                <p className="flex items-center">
+                    <Twitter size="14px" className="mr-1"/>
+                    <span className="ml-2">Tweet your favorite quote</span>
+                </p>
+                <p className="flex items-center">
+                    <Link2 size="14px" className="mr-1"/>
+                    <span className="ml-2">Copy this website&apos;s URL to share</span>
+                </p>
+            </CardContent>
+            <CardContent>
+                <p className="flex items-center">
+                    <Github className="mr-1"/>
+                    <span className="ml-2 underline"><a href="https://github.com/loganvrankovic">https://github.com/loganvrankovic</a></span>
+                </p>
+            </CardContent>
+            <CardContent className="text-center pt-2 mb-[-20px]">
+                <Button onClick={onClose} variant="ghost"><XCircle /></Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+// random quote div -------------------------------------------------
+export default function RQuote() {
+    const [quoteState, setQuoteState] = useState(quoteRand());
+    const [bgColor, setBgColor] = useState<string>("#7E9181")
+    const [isFading, setIsFading] = useState(false);
+    const [showInstructions, setShowInstructions] = useState(false);
+
+    const refreshQuote = () => {
+        setIsFading(true)
+
+        setTimeout(() => {
+            const newQuote = quoteRand();
+            const newBgColor = getColor();
+            setBgColor(newBgColor);
+            setQuoteState(newQuote);
+            setIsFading(false);
+        }, 500)
+    }
+
+    const toggleInstructions = () => {
+        setShowInstructions(!showInstructions);
+    }
+
+    const getColor = () => {
+        return quoteState[1] === "public/irohmp3/iroh0.mp3" ? "#2E3532" :
+            quoteState[1] === "irohmp3/iroh1.mp3" ? "#C7CEDB" :
+            quoteState[1] === "irohmp3/iroh2.mp3" ? "#94849B" :
+            quoteState[1] === "irohmp3/iroh3.mp3" ? "#A0AAB2" :
+            quoteState[1] === "irohmp3/iroh4.mp3" ? "#d5ff80" :
+            quoteState[1] === "irohmp3/iroh5.mp3" ? "#242F40" :
+            quoteState[1] === "irohmp3/iroh6.mp3" ? "#F9E784" :
+            quoteState[1] === "irohmp3/iroh7.mp3" ? "#685044" :
+            quoteState[1] === "irohmp3/iroh8.mp3" ? "#8075FF" :
+            quoteState[1] === "irohmp3/iroh9.mp3" ? "#E5D1D0" :
+            quoteState[1] === "irohmp3/iroh10.mp3" ? "#ccccff" :
+            "#2A2C24";
+    }
+
+    interface AudioPlayerProps {
+     audioFile: string;
+    }
+
+    const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioFile }) => {
+        const audioRef = useRef<HTMLAudioElement>(null);
+
+        const playAudio = () => {
+         console.log(audioRef.current)
+            if (audioRef.current) {
+                audioRef.current.play();
+            }
+        }
+
+        return (
+            <div>
+                <audio ref={audioRef} src={audioFile} />
+                <Button onClick={playAudio} className="share head absolute top-5" variant="outline">
+                    <Headphones className="h-5 w-5" />
+                </Button>
+            </div>
+        )
+    }
 
     return (
         <div className="flex items-center justify-center h-screen mx-auto" style={{ backgroundColor: bgColor, transition: "background-color 1s" }}>
@@ -159,10 +248,11 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioFile }) => {
                     </a>
                     <CopyURLButton />
                     <div className="relative ml-3 text-xs md:text-xs lg:text-sm xl:text-sm">
-                        <Help />
+                        <Help onClick={toggleInstructions} />
                     </div>
                 </CardFooter>
             </Card>
+            { showInstructions && (<InstructionsCard onClose={toggleInstructions} />) }
         </div>
     )
 }
