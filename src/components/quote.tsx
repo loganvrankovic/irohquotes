d60a84c81ee6b6434d5755e11c37f49e42f89d74
Logan Vrankovic <loganvrz@gmail.com>
2023-11-28 09:07:04 -0500

    added transitions, horizontal styling for info card

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -101,39 +101,45 @@ function InstructionsCard({ onClose }: InstructionsCardProps) {
                 <Info size="16px" className="mr-1" />
                 info
             </p></CardTitle>
-            <CardDescription className="pt-6">top-right buttons</CardDescription>
-            <CardContent className="pt-2">
-                <p className="flex items-center">
-                    <RotateCw size="14px" className="mr-1"/>
-                    <span className="ml-2">refresh the quote and mood</span>
-                </p>
-                <p className="flex items-center">
-                    <Headphones size="14px" className="mr-1"/>
-                    <span className="ml-2">listen to Iroh&apos;s words of wisdom</span>
-                </p>
-            </CardContent>
-            <CardDescription>bottom-left buttons</CardDescription>
-            <CardContent className="pt-2 sm:text-xs md:text-sm lg:text-base xl:text-md">
-                <p className="flex items-center">
-                    <Sun size="14px" className="mr-1"/>
-                    <span className="ml-2">change the quote card&apos;s theme</span>
-                </p>
-                <p className="flex items-center">
-                    <Coffee size="14px" className="mr-1"/>
-                    <span className="ml-2">learn about different tea that Iroh enjoys</span>
-                </p>
-                <p className="flex items-center">
-                    <Twitter size="14px" className="mr-1"/>
-                    <span className="ml-2">Tweet your favorite quote</span>
-                </p>
-                <p className="flex items-center">
-                    <Link2 size="14px" className="mr-1"/>
-                    <span className="ml-2">copy this website&apos;s URL to share</span>
-                </p>
-            </CardContent>
+            <div className="infohori">
+                <div className="info1">
+                    <CardDescription className="pt-6">top-right buttons</CardDescription>
+                    <CardContent className="pt-2 sm:text-xs md:text-sm lg:text-base xl:text-md">
+                        <p className="flex items-center">
+                            <RotateCw size="14px" className="mr-1"/>
+                            <span className="ml-2">refresh the quote and mood</span>
+                        </p>
+                        <p className="flex items-center">
+                            <Headphones size="14px" className="mr-1"/>
+                            <span className="ml-2">listen to Iroh&apos;s words of wisdom</span>
+                        </p>
+                    </CardContent>
+                </div>
+                <div className="info2">
+                <CardDescription>bottom-left buttons</CardDescription>
+                <CardContent className="pt-2 sm:text-xs md:text-sm lg:text-base xl:text-md">
+                    <p className="flex items-center">
+                        <Sun size="14px" className="mr-1"/>
+                        <span className="ml-2">change the quote card&apos;s theme</span>
+                    </p>
+                    <p className="flex items-center">
+                        <Coffee size="14px" className="mr-1"/>
+                        <span className="ml-2">learn about different tea that Iroh enjoys</span>
+                    </p>
+                    <p className="flex items-center">
+                        <Twitter size="14px" className="mr-1"/>
+                        <span className="ml-2">Tweet your favorite quote</span>
+                    </p>
+                    <p className="flex items-center">
+                        <Link2 size="14px" className="mr-1"/>
+                        <span className="ml-2">copy this website&apos;s URL to share</span>
+                    </p>
+                </CardContent>
+                </div>
+            </div>
             <CardContent>
-                <p className="flex items-center">
-                    <Github className="mr-1"/>
+                <p className="github flex items-center">
+                    <Github className="mr-1 sm:text-xs md:text-sm lg:text-base xl:text-md"/>
                     <span className="ml-2 underline"><a href="https://github.com/loganvrankovic">https://github.com/loganvrankovic</a></span>
                 </p>
             </CardContent>
